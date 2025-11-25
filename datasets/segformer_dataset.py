214c89443c50cdfbf11f3dc418c14723d1352ef2
abeshahsan <abeshahsan2002@gmail.com>
2025-11-26 02:15:20 +0600

    Enhance error handling in SegformerDataset and SegformerTransform; add SegformerLitWrapper for model training and validation

--- datasets/segformer_dataset.py
+++ datasets/segformer_dataset.py
@@ -7,32 +7,39 @@ import torchvision.transforms as T
 import torchvision.transforms.functional as TF
 
 from datasets.transforms import SegformerTransform
+from exceptions import DatasetException
 
 
 class SegformerDataset(Dataset):
     def __init__(
         self, root, img_dir, mask_dir, transforms: Union[SegformerTransform, T.Compose]
     ):
-        self.root = root
-        self.img_dir = os.path.join(root, img_dir)
-        self.mask_dir = os.path.join(root, mask_dir)
+        try:
+            self.root = root
+            self.img_dir = os.path.join(root, img_dir)
+            self.mask_dir = os.path.join(root, mask_dir)
 
-        self.images = sorted(os.listdir(self.img_dir))
-        self.masks = sorted(os.listdir(self.mask_dir))
+            self.images = sorted(os.listdir(self.img_dir))
+            self.masks = sorted(os.listdir(self.mask_dir))
 
-        self.transforms = transforms
+            self.transforms = transforms
+        except Exception as e:
+            raise DatasetException(f"Failed to initialize: {str(e)}") from e
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.images[idx])
-        mask_path = os.path.join(self.mask_dir, self.masks[idx])
+        try:
+            img_path = os.path.join(self.img_dir, self.images[idx])
+            mask_path = os.path.join(self.mask_dir, self.masks[idx])
 
-        img = Image.open(img_path).convert("RGB")
-        mask = (
-            np.array(Image.open(mask_path), dtype=np.uint8) - 1
-        )  # Adjust mask labels to start from 0
-        img, mask = self.transforms(images=img, masks=mask)
+            img = Image.open(img_path).convert("RGB")
+            mask = (
+                np.array(Image.open(mask_path), dtype=np.uint8) - 1
+            )  # Adjust mask labels to start from 0
+            img, mask = self.transforms(images=img, masks=mask)
 
-        return img, mask
+            return img, mask
+        except Exception as e:
+            raise DatasetException(f"loading index {idx}: {str(e)}") from e
 
     def __len__(self):
         return len(self.images)
